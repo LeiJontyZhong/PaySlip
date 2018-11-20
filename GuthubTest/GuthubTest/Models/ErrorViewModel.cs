4f02c52278fd44f04c77de5ae0e3b170c1306b4e
jonty <jontyispro@hotmail.com>
2018-11-20 20:02:24 +1300

    Init committment

---
+++ GuthubTest/GuthubTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuthubTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
